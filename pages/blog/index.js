d4401c51bff8566eb25523ae445c8a109ac34121
sajid365-sr <sajidsorker86@gmail.com>
2022-12-20 22:08:14 +0600

    Link, Head and Image component setup

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,16 @@
 
 import React from 'react';
+import Link from "next/link";
+import Head from 'next/head';
 
 const Blog = () => {
     return (
         <div>
+            <Head>
+                <title>Blog Page</title>
+            </Head>
             <h2>This is blog page</h2>
+            <Link href="/blog/blog1">Blog Page 1</Link>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import Head from 'next/head';
+import Image from "next/image";
+
 
 
 export default function Home() {
+
   return (
-    <>
+    <div>
+     <Head>
+        <title>Home</title>
+     </Head>
       <h1>Hello Next Js</h1>
-      
-    </>
-  )
+      <Link href="/blog" style={{backgroundColor:'orange', padding:'5px 10px', borderRadius:'5px'}}>blog</Link>
+
+      <Image src="/nature.jpg" width={500} height={300}></Image>
+    </div>
+  );
 }
